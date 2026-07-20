87402acc94aa733925bf2b41da8e854dda980117
mgayathri-git <mgayathri20009@gmail.com>
2026-07-20 11:08:22 +0530

    testing

--- SPH-ApiGateway/src/main/java/com/gateway/smart/pay/hub/sph/config/SecurityConfig.java
+++ SPH-ApiGateway/src/main/java/com/gateway/smart/pay/hub/sph/config/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtGra
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig  {
 
     @Bean
     @ConditionalOnMissingBean
